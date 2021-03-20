95fcfdd3d2f7d7ed2dc9dce8f28cb615f203d643
Drew Weston <awfiscus@comcast.net>
2021-03-20 16:29:42 -0600

    added base offline db functions to db.js

---
+++ public/db.js
@@ -0,0 +1,26 @@
+let db;
+
+const request = indexedDB.open("expenses", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Something Went Wrong " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
